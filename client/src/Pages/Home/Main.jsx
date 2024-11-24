0a56ab00ab6d5a59c441b8758cdf5713a49ffb61
tomarmayankk <mayanktomarpc@gmail.com>
2024-11-24 15:06:13 +0530

    Main

--- client/src/Pages/Home/Main.jsx
+++ client/src/Pages/Home/Main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Main = () => {
   return (
@@ -7,18 +8,16 @@ const Main = () => {
       <p className="text-lg text-gray-600 mb-8">Your personal space to organize and manage notes</p>
       
       <div className="flex space-x-4">
-        <button
+        <Link to = "/Login"
           className="px-6 py-2 text-white bg-blue-500 hover:bg-blue-600 rounded-md"
-          onClick={() => console.log("Navigate to Login")}
         >
           Login
-        </button>
-        <button
+        </Link>
+        <Link to = "/register"
           className="px-6 py-2 text-white bg-green-500 hover:bg-green-600 rounded-md"
-          onClick={() => console.log("Navigate to Sign Up")}
         >
           Sign Up
-        </button>
+        </Link>
       </div>
     </div>
   );
